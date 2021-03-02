0cdb0ed1d71b032abd8536c3356d1255810dfafa
Austin Caron <austin.caron1@gmail.com>
2021-03-01 22:24:14 -0500

    added functionality for filter btns on feed page. added basic template content

---
+++ src/components/feed/FeedArticle.vue
@@ -0,0 +1,17 @@
+<template>
+  <router-link :to="'post/' + article.uid" class="feedArticle">
+      <prismic-image :field="article.data.image" />
+      <h2>{{ article.data.title[0].text }}</h2>
+  </router-link>
+</template>
+
+<script>
+export default {
+props: ['article'],
+
+}
+</script>
+
+<style>
+
+</style>
