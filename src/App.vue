e43117d7c68b3a278ca190854082c88b17b0c0c3
Austin Caron <austin.caron1@gmail.com>
2021-03-07 09:49:08 -0500

    messing around with seo

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,11 @@
 <script>
 import Navigation from './components/Navigation.vue';
 export default {
+  name: 'App',
+  metaInfo: {
+    title: 'Subjective Speech'
+    
+  },
 
   components: {
     Navigation: Navigation
@@ -20,6 +25,7 @@ export default {
 </script>
 
 <style>
+@import '../public/css/main.css';
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
